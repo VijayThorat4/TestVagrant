342514184b97799e87b4f56f218d4b4ab3e347fd
unknown <vijaysinhathorat@yahoo.com>
2020-11-19 17:50:53 +0530

    Completed POM for home page

--- Temperature_Comparator/NDTV_Weather/src/main/java/com/NDTV/Pages/HomePage.java
+++ Temperature_Comparator/NDTV_Weather/src/main/java/com/NDTV/Pages/HomePage.java
@@ -1,10 +1,31 @@
 package com.NDTV.Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class HomePage {
+	WebDriver driver;
+	
+	// All locators belonging to home page that are required for this project
 	By initialPopup = By.xpath("//a[text()=\"No Thanks\"]");
 	By menuExpander = By.id("h_sub_menu");
 	By weatherOption = By.xpath("//a[text()=\"WEATHER\"]");
+	
+	// Constructor to initialize WebDriver
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	// Method to perform actions on locators
+	public void dismissInitialPopup() {
+		driver.findElement(initialPopup).click();
+	}
+	
+	public void expandMenu() {
+		driver.findElement(menuExpander).click();
+	}
 
+	public void selectWeather() {
+		driver.findElement(weatherOption).click();
+	}
 }
--- Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Temperature_GUI.java
+++ Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Temperature_GUI.java
@@ -1,5 +1,24 @@
 package Tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.NDTV.Pages.HomePage;
+
 public class Temperature_GUI {
+	
+	@Test
+	public void Fetch_GUI_Temp() {
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver2.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.ndtv.com/");
+		driver.manage().window().maximize();
+		
+		HomePage home = new HomePage(driver);
+		home.dismissInitialPopup();
+		home.expandMenu();
+		home.selectWeather();
+	}
 
 }
