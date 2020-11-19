cc508a43a8b50056f13d02ab98863e03a1824a7c
unknown <vijaysinhathorat@yahoo.com>
2020-11-19 16:36:26 +0530

    Added POM locators

--- Temperature_Comparator/NDTV_Weather/src/main/java/com/NDTV/Pages/HomePage.java
+++ Temperature_Comparator/NDTV_Weather/src/main/java/com/NDTV/Pages/HomePage.java
@@ -1,5 +1,10 @@
 package com.NDTV.Pages;
 
+import org.openqa.selenium.By;
+
 public class HomePage {
+	By initialPopup = By.xpath("//a[text()=\"No Thanks\"]");
+	By menuExpander = By.id("h_sub_menu");
+	By weatherOption = By.xpath("//a[text()=\"WEATHER\"]");
 
 }
--- Temperature_Comparator/NDTV_Weather/src/main/java/com/NDTV/Pages/WeatherClass.java
+++ Temperature_Comparator/NDTV_Weather/src/main/java/com/NDTV/Pages/WeatherClass.java
@@ -1,5 +1,10 @@
 package com.NDTV.Pages;
 
-public class WeatherClass {
+import org.openqa.selenium.By;
 
+public class WeatherClass {
+	By searchBox = By.id("searchBox");
+	By cityCheckboxSelector = By.id("Pune");
+	By cityOnMap = By.xpath("//div[text()=\"Pune\"]");
+	By tempLocator = By.xpath("//div[@class=\"leaflet-popup-content\"]/div/span[4]");
 }
