4c64f963d3bcf9b99ad3514d974680710f2abec8
unknown <vijaysinhathorat@yahoo.com>
2020-11-22 19:19:16 +0530

    Implementation completed

--- Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Base.java
+++ Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Base.java
@@ -1,14 +1,9 @@
 package Tests;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler;
 
 public class Base {
 	public static WebDriver driver;
@@ -19,11 +14,5 @@ public class Base {
 		driver.get("https://www.ndtv.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	}
-	
-	public void failed(String methodName) throws IOException {
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File snap = ts.getScreenshotAs(OutputType.FILE);
-		FileHandler.copy(snap, new File ("E:\\Project\\Temperature_Comparator\\NDTV_Weather\\Screenshot" + methodName + "_failed.jpg"));
 	}
 }
--- Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Temperature_GUI.java
+++ Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Temperature_GUI.java
@@ -1,9 +1,5 @@
 package Tests;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
