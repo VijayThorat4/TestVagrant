b765f66b7b66358801509e2dfb06a9fc9f84f9b1
unknown <vijaysinhathorat@yahoo.com>
2020-11-22 10:45:06 +0530

    Comparator class completed

--- Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Temperature_Compare.java
+++ Temperature_Comparator/NDTV_Weather/src/test/java/Tests/Temperature_Compare.java
@@ -6,11 +6,25 @@ import org.testng.annotations.Test;
 
 public class Temperature_Compare {
 	
+	
+	@Test
 	public void compare_Values() {
 		
-		float GUI_Temp = Float.parseFloat(Temperature_GUI.temp_GUI);
-		float API_Temp = Float.parseFloat(Temperature_API.temp_api);
+//		Temperature_GUI.temp_GUI = "30";
+//		Temperature_API.temp_api = "303";
 		
+		// Converting String values to double data type
+		double GUI_Temp = Float.parseFloat(Temperature_GUI.temp_GUI);
+		double API_Temp = Float.parseFloat(Temperature_API.temp_api);
+		
+		GUI_Temp = GUI_Temp + 273.15;   //Converting from celsius to kelvin
+		
+		if (Math.abs(GUI_Temp - API_Temp) < 10) {
+			System.out.println("Temerature Variation is within limit");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("Temperature variation is more than the expected limit");
+			Assert.assertTrue(false);
+		}
 	}
-
 }
